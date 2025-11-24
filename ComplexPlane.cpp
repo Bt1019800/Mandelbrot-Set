ff64a32d64256596f26e0282426e27e12a6c6b7b
Your Name <you@example.com>
2025-11-23 17:15:39 -0800

    Partner: Tejbir Sidhu

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -1,5 +1,6 @@
 #include "ComplexPlane.h"
 #include <complex>
+#include <cmath>
 #include <SFML/Graphics/VertexArray.hpp>
 
 ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
@@ -19,10 +20,34 @@ void ComplexPlane::draw(RenderTarget& target, RenderStates states) const
     target.draw(m_vArray);
 }
 
+void ComplexPlane::zoomIn()
+{
+    m_zoomCount++;
+    int xSize = BASE_WIDTH * (pow(BASE_ZOOM, m_ZoomCount));
+    int ySize = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM, m_ZoomCount));
+    m_plane_size = {xSize, ySize};
+    m_state = CALCULATING;
+}
+
+void ComplexPlane::zoomOut()
+{
+    m_zoomCount--;
+    int xSize = BASE_WIDTH * (pow(BASE_ZOOM, m_ZoomCount));
+    int ySize = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM, m_ZoomCount));
+    m_plane_size = {xSize, ySize};
+    m_state = CALCULATING;    
+}
+
 void complexPlane::updateRender()
 {
     if (m_state == CALCULATING)
     {
-        for ()
+        for (int i = 0; i < ; i++)
+        {
+            for (int j = 0; j < ; j++)
+            {
+
+            }
+        }
     }
 }
