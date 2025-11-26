c358e5de0cfe80c3a9eadb8c7a91eba8c5cc3c77
Your Name <you@example.com>
2025-11-25 23:47:51 -0800

    Partner Name: Tejbir Sidhu

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -1,8 +1,10 @@
 #include "ComplexPlane.h"
 #include <complex>
-#include <sstream>
 #include <cmath>
+#include <sstream>
 #include <SFML/Graphics/VertexArray.hpp>
+using namespace std;
+using namespace sf;
 
 ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
 {
@@ -16,7 +18,7 @@ ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
     m_vArray.resize(pixelWidth * pixelHeight);
 }
 
-void ComplexPlane::draw(RenderTarget& target, RenderStates states) const
+void ComplexPlane::draw(sf:: RenderTarget& target, sf:: RenderStates states) const
 {
     target.draw(m_vArray);
 }
@@ -24,40 +26,40 @@ void ComplexPlane::draw(RenderTarget& target, RenderStates states) const
 void ComplexPlane::zoomIn()
 {
     m_zoomCount++;
-    int xSize = BASE_WIDTH * (pow(BASE_ZOOM, m_ZoomCount));
-    int ySize = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM, m_ZoomCount));
+    float xSize = BASE_WIDTH * (pow(BASE_ZOOM, m_zoomCount));
+    float ySize = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM, m_zoomCount));
     m_plane_size = {xSize, ySize};
-    m_state = State::CALCULATING;
+    m_state = State:: CALCULATING;
 }
 
 void ComplexPlane::zoomOut()
 {
     m_zoomCount--;
-    int xSize = BASE_WIDTH * (pow(BASE_ZOOM, m_ZoomCount));
-    int ySize = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM, m_ZoomCount));
+    float xSize = BASE_WIDTH * (pow(BASE_ZOOM, m_zoomCount));
+    float ySize = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM, m_zoomCount));
     m_plane_size = {xSize, ySize};
-    m_state = State::CALCULATING;    
+    m_state = State:: CALCULATING;    
 }
 
-void complexPlane::updateRender()
+void ComplexPlane::updateRender()
 {
     if (m_state == State::CALCULATING)
     {
-        for (int i = 0; i < m_plane_size.y; i++)
+        for (int i = 0; i < m_pixel_size.y ; i++)
         {
-            for (int j = 0; j < m_plane_size.x; j++)
+            for (int j = 0; j < m_pixel_size.x; j++)
             {
-                int index = i + j*m_pixel_size.x;
-                vArray[index].position = { (float)j,(float)i };
-                Vector2f coord = mapPixelToCoords ( Vector2i(j,i));
+                int index = j + i*m_pixel_size.x;
+                m_vArray[index].position = {(float)j,float(i)};
+                Vector2f coord = mapPixelToCoords(Vector2i(j,i));
                 size_t numIterations = countIterations(coord);
+
                 Uint8 r, g, b;
-                iterationsToRGB (numIterations, r, g, b);
-                vArray[j + i * pixelWidth].color = { r,g,b };
+                iterationsToRGB(numIterations, r, g, b);
+                m_vArray[j + i * m_pixel_size.x].color = {r,g,b};
             }
         }
-
-        m_state = State::DISPLAYING;
+        m_state = State:: DISPLAYING;
     }
 }
 
@@ -66,7 +68,7 @@ void ComplexPlane::setCenter(Vector2i mousePixel)
     Vector2f screenPixelLocation;
     screenPixelLocation = mapPixelToCoords(mousePixel);
     m_plane_center = screenPixelLocation;
-    m_State = State::CALCULATING;   
+    m_state = State:: CALCULATING;
 }
 
 void ComplexPlane::setMouseLocation(Vector2i mousePixel)
@@ -88,17 +90,73 @@ void ComplexPlane::loadText(Text& text)
     text.setString(ss.str());
 }
 
-size_t ComplexPlane::countIterations(Vector2f coord)
+size_t ComplexPlane:: countIterations(Vector2f coord)
 {
+    complex<float> c (coord.x, coord.y);
+    complex<float> z (0,0);
 
+    for(size_t iteration = 0; iteration < MAX_ITER; iteration++)
+    {
+        z = z * z + c;
+        if(abs(z) > 2.0)
+        {
+            return iteration;
+        }
+    }
+    return MAX_ITER; //holds same value as iteration but since we can't access we used MAX_ITER instead
+}
+void ComplexPlane:: iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
+{
+    if(count == MAX_ITER)
+    {
+        r = 0;
+        g = 0;
+        b = 0;
+    }
+    else if(count < MAX_ITER)
+    {
+        if(count <= 13)
+        {
+            r = 203;
+            g = 8;
+            b = 247;
+        }
+        else if(count <= 26)
+        {
+            r = 8;
+            g = 171;
+            b = 247;
+        }
+        else if(count <= 39)
+        {
+            r = 8;
+            g = 247;
+            b = 84;
+        }
+        else if(count <= 52)
+        {
+            r = 243;
+            g = 247;
+            b = 8;
+        }
+        else
+        {
+            r = 247;
+            g = 24;
+            b = 8;
+        }
+    }
+}
+Vector2f ComplexPlane:: mapPixelToCoords(Vector2i mousePixel)
+{
+    float realMin = m_plane_center.x-m_plane_size.x/2.0f;
+    float realMax = m_plane_center.x + m_plane_size.x/2.0f;
+    float realNum = ((float)(mousePixel.x - 0)/(float)(m_pixel_size.x -0)) * (realMax - realMin) + realMin;
+
+    float imagMin = m_plane_center.y - m_plane_size.y/2.0f;
+    float imagMax = m_plane_center.y + m_plane_size.y/2.0f;
+    float imagNum = ((float)(mousePixel.y - m_pixel_size.y)/(float)(0 - m_pixel_size.y)) * (imagMax - imagMin) + imagMin;
+
+    return Vector2f(realNum, imagNum);
 }
 
-void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
-{
-
-}
-
-Vector2f ComplexPlane::mapPixelToCoords(Vector2i mousePixel)
-{
-    
-}
--- main.cpp
+++ main.cpp
@@ -1,68 +1,73 @@
 #include "ComplexPlane.h"
-#include <SFML/Graphics.hpp>
-#include <sstream>
-#include <cmath>
+#include <SFML/Graphics/VertexArray.hpp>
+#include <complex>
 #include <iostream>
 using namespace std;
 using namespace sf;
 
 int main()
 {
-    int width, height;
+    int height, width;
     width = VideoMode::getDesktopMode().width;
     height = VideoMode::getDesktopMode().height;
+
     VideoMode vm(width, height);
-
-    RenderWindow window(vm, "Mandelbrot", Style::Default);
-
-    ComplexPlane plane (width, height);
-
+    RenderWindow window(vm, "MandelbrotSet");
+    ComplexPlane plane(width, height);
     Font font;
 
-	if (!font.loadFromFile("batman.ttf"))
+    if(!font.loadFromFile("JumpsWinter.ttf"))
+    {
+        cout << "Error, couldn't open font file" << endl;
+    }
 
-	{
-		cout << "Error, couldn't open font file" << endl;
-	}
-
-	Text text;
-	text.setFont(font);
+    Text text;
+    text.setFont(font);
     text.setCharacterSize(24);
-	text.setFillColor(Color::White);
+    text.setFillColor(Color::White);
 
     while (window.isOpen())
-	{
+    {
         Event event;
-		while (window.pollEvent(event))
-		{
-		    if (event.type == Event::Closed)
-		    {
+        while(window.pollEvent(event))
+        {
+            if(event.type == Event::Closed)
+            {
                 window.close();
             }
-            if (event.type == sf::Event::MouseButtonPressed)
-		    {
-                if (event.mouseButton.button == sf::Mouse::Left)
+            
+            if(event.type == Event::MouseButtonPressed)
+            {
+                if(event.mouseButton.button == Mouse::Left)
                 {
                     plane.zoomIn();
-                    plane.setCenter(Vector2f(event.mouseButton.x, event.mouseButton.y));
+                    plane.setCenter(Vector2i(event.mouseButton.x, event.mouseButton.y));
                 }
-                else if (event.mouseButton.button == sf::Mouse::Right)
+                else if(event.mouseButton.button == Mouse::Right)
                 {
                     plane.zoomOut();
-                    plane.setCenter(Vector2f(event.mouseButton.x, event.mouseButton.y));
+                    plane.setCenter(Vector2i(event.mouseButton.x,event.mouseButton.y));
                 }
             }
 
-            if (event.type == sf::Event::MouseMoved)
+            if(event.type == Event:: MouseMoved)
             {
-                plane.setMouseLocation(Vector2f(event.mouseButton.x, event.mouseButton.y));
+                plane.setMouseLocation(Vector2i(event.mouseButton.x,event.mouseButton.y));
             }
             
-            if (Keyboard::isKeyPressed(Keyboard::Escape))
-		    {
-			    window.close();
-		    }
+            if(Keyboard::isKeyPressed(Keyboard::Escape))
+            {
+                window.close();
+            }
+        }
+        plane.updateRender();
+        plane.loadText(text);
+        window.clear();
+        window.draw(plane);
+        window.draw(text);
+        window.display();    
     }
 
     return 0;
+
 }
